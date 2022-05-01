4f209196f8630421c5654014260f05bc79238276
Pontus <pontus.g91@gmail.com>
2022-05-01 16:41:57 +0200

    added sound to spawnpoint

--- gamelevel/Main.gd
+++ gamelevel/Main.gd
@@ -1,14 +1,17 @@
 extends Node
 
 var enemies = preload("res://Actors/Enemy/monster.tscn")
+onready var spawn1 = $Spawner
+onready var spawnsound2 = $Spawner/spawnsound
 
 func _on_SpawnTimer_timeout():
 	var enemy = enemies.instance()
 	add_child(enemy)
 	enemy.position = $Spawner.position
-
+	spawnsound2.play()
 
 func _on_SpawnTimer2_timeout():
 	var enemy = enemies.instance()
 	add_child(enemy)
 	enemy.position = $Spawner2.position
+	spawnsound2.play()
